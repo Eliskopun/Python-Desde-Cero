2ebd51cc357713e516064dc399ef279a990bed76
Eliskopun <eliezeravilan@gmail.com>
2024-11-27 21:23:04 -0500

    Se agregan archivos para Leccion 03 - Operadores

---
+++ 02_operators.py
@@ -0,0 +1,310 @@
+### Operadores Aritméticos ###
+
+# Operaciones con enteros
+print(3 + 4) # suma
+print(3 - 4) # resta
+print(3 * 4) # multiplicacion
+print(3 / 4) # division
+print(10 % 3) # modulo
+print(10 // 3) # division de suelo
+print(2 ** 3) # potencia o exponente
+print(2 ** 3 + 3 - 7 / 1 // 4) # multiples operaciones
+
+# Operaciones con cadenas de texto
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
+
+# Operaciones mixtas
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+# Operaciones con enteros
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
+
+# Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
+
+
+# Dia 3
+
+print(True)
+print(False)
+
+# Arithmetic Operations in Python
+# Integers
+
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 8 it means 2 * 2 * 2
+
+# Floating numbers
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+
+# Declaring the variable at the top first
+
+a = 3 # a is a variable name and 3 is an integer data type
+b = 2 # b is a variable name and 3 is an integer data type
+
+# Arithmetic operations and assigning the result to a variable
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+# I should have used sum instead of total but sum is a built-in function - try to avoid overriding built-in functions
+print(total) # if you do not label your print with some string, you never know where the result is coming from
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
+
+print('== Addition, Subtraction, Multiplication, Division, Modulus ==')
+
+# Declaring values and organizing them together
+num_one = 3
+num_two = 4
+
+# Arithmetic operations
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+div = num_two / num_one
+remainder = num_two % num_one
+
+# Printing values with label
+print('total: ', total)
+print('difference: ', diff)
+print('product: ', product)
+print('division: ', div)
+print('remainder: ', remainder)
+
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')                         # Adding unit to the weight
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
+
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')                         # Adding unit to the weight
+
+# Calculate the density of a liquid
+mass = 75 # in Kg
+volume = 0.075 # in cubic meter
+density = mass / volume # 1000 Kg/m^3
+
+print(3 > 2)     # True, because 3 is greater than 2
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False,  because 3 is greater than 2
+print(2 < 3)     # True, because 2 is less than 3
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))  # False
+print(len('mango') != len('avocado'))  # True
+print(len('mango') < len('avocado'))   # True
+print(len('milk') != len('meat'))      # False
+print(len('milk') == len('meat'))      # True
+print(len('tomato') == len('potato'))  # True
+print(len('python') > len('dragon'))   # False
+
+
+# Comparing something gives either a True or False
+
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+
+print('1 is 1', 1 is 1)                   # True - because the data values are the same
+print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
+print('B in Asabeneh', 'B' in 'Asabeneh') # False - there is no uppercase B
+print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('a in an:', 'a' in 'an')      # True
+print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+print('Eliezer not in Eli:', 'Eliezer' not in 'Eli')
+
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
+
+# Ejercicios - Dia 3
+
+age = int(25) # 1
+height = float(1.75) # 2
+num_complex = 1j # 3
+
+# 4
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+print("The area of the triangle is:", int(0.5 * base * height))
+
+# 5 
+a= int(input("Enter side a: "))
+b= int(input("Enter side b: "))
+c= int(input("Enter side c: "))
+print("The perimeter of the triangle is:", a + b + c)
+
+# 6
+length = int(input("Enter length of rectangle: "))
+width = int(input("Enter width of rectangle: "))
+area_of_rectangle = length * width
+perimeter_of_rectangle = 2 * (length + width)
+print("The area of the rectangle:", area_of_rectangle)
+print("The perimeter of the rectangle:", perimeter_of_rectangle)
+
+# 7
+pi = 3.14
+radius_of_circle = input("Enter the radius of circle: ")
+print(f"Area of circle: {pi * float(radius_of_circle) * float(radius_of_circle):.1f}")
+print(f"Circumference of circle: {2 * pi * float(radius_of_circle):.1f}")
+
+# 8
+m = 2
+x = 0
+y = 0
+y_intercept = 2 * x - 2
+x_intercept = (y + 2) / 2
+print(f"La pendiente es: {m}\n El intercepto en X es: {x_intercept:.0f}\n El intercepto en Y es: {y_intercept}")
+
+# 12
+str_1 = "Python"
+str_2 = "Dragon"
+print(f"{str_1} is different a {str_2}:", len(str_1) != len(str_2))
+
+# 13
+print("on" in str_1 and "on" in str_2)
+
+# 14
+sentence = "I hope this course is not full of jargon"
+print("jargon" in sentence)
+
+# 15
+print("on" not in str_1 and "on" not in str_2)
+
+# 16
+print(type(str(float(len("Python")))))
+
+# 17
+num = int(input("Enter a number to check if it is even or not using Python: "))
+print(f"This number {num} is even: {num % 2 == 0}")
+
+# 18
+print(7 // 3 == int(2.7))
+
+# 19
+print(type('10') == type(10))
+
+# 20
+print(int('9.8') == 10)
+
+# 21
+hours = input("Enter hours: ")
+rate_per_hour = input("Enter rate per hour: ")
+print("Your weekly earning is:",  hours * rate_per_hour)
+
+# 22
+years_lived = input("Enter number of years you have lived: ")
+print(f"You have lived for {int(years_lived) * 31536000} seconds")
+
+# 23
+print("""1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125""")
+
